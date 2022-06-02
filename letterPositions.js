edf6ac6bb6d935d54297cf4650e4d65a6cd9f51b
Aky Mac Donald <kotoko@Kotoko.local>
2022-06-02 13:13:04 -0400

    fully functional letterPositions; needs cleanup

--- letterPositions.js
+++ letterPositions.js
@@ -32,41 +32,36 @@ const assertArraysEqual = function(arrayOne, arrayTwo) {
 //returns all the INDICES in the string where the given character is found
 //multiple numbers may be needed to represent all the places in the string
 
+//reference guide: letter (key): `sentence[i]`, index (value): `i`
+
 const letterPositions = function(sentence) {
   let results = {};
 
+  // add something to skip spaces
+
   for (let i = 0; i < sentence.length; i++) {
-    if (results[i] === undefined) {
-      results += [sentence[i], i];
-   
+
+    //   //key doesn't exist
+    //   if (results[sentence[i]] === undefined) {
+    //     results[sentence[i]] = [i];
+
+    //     //key exists
+    //   } else {
+    //     results[sentence[i]].push(i);
+    //   }
+    if (sentence[i] !== " ") {
+      results[sentence[i]] ? results[sentence[i]].push(i) : results[sentence[i]] = [i];
     }
-    console.log("letter: ", sentence[i], "letter index: ", i, "results: ", results);
+
   }
-  
   return results;
 };
 
 
-//simple test
-console.log(letterPositions("hello"));
-// assertArraysEqual(letterPositions("hello").h, [0]);
-// assertArraysEqual(letterPositions("hello").e, [1]);
-// assertArraysEqual(letterPositions("hello").l, [2, 3]);
-// assertArraysEqual(letterPositions("hello").o, [4]);
+//test
+assertArraysEqual(letterPositions("hello hi").h, [0, 6]);
+assertArraysEqual(letterPositions("hello hi").e, [1]);
+assertArraysEqual(letterPositions("hello hi").l, [2, 3]);
+assertArraysEqual(letterPositions("hello hi").o, [4]);
+assertArraysEqual(letterPositions("hello hi").o, [4]);
 
-//big test
-//console.log(letterPositions("lighthouse in the house"));
-
-// expected result:
-// {
-//   l: [0],
-//   i: [1, 11],
-//   g: [2],
-//   h: [3, 5, 15, 18],
-//   t: [4, 14],
-//   o: [6, 19],
-//   u: [7, 20],
-//   s: [8, 21],
-//   e: [9, 16, 22],
-//   n: [12]
-// }
